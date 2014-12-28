5033a4e78037bb56005ef8b1d98123def79cd8de
Juergen Eckert <juergen@familie-eckert.de>
2014-12-28 18:58:11 +0100

    base schematic

---
+++ firmware-plain/include/src/json/.output/eagle/release/obj/jsonparse.d
@@ -0,0 +1,1 @@
+.output/eagle/release/obj/jsonparse.o .output/eagle/release/obj/jsonparse.d : jsonparse.c
---
+++ firmware-plain/src/lwip/app/.output/eagle/release/obj/ping.d
@@ -0,0 +1,23 @@
+.output/eagle/release/obj/ping.o .output/eagle/release/obj/ping.d : ping.c ../../include/lwip/opt.h ../../include/lwipopts.h \
+ ../../include/lwip/debug.h ../../include/lwip/arch.h \
+ ../../include/arch/cc.h ../../../include/c_types.h \
+ ../../../include/ets_sys.h ../../../include/c_types.h \
+ ../../../include/eagle_soc.h ../../../include/osapi.h \
+ ../../../include/string.h ../../../include/_ansi.h \
+ ../../../include/newlib.h ../../../include/sys/config.h \
+ ../../../include/machine/ieeefp.h ../../../include/sys/reent.h \
+ ../../../include/_ansi.h ../../../include/sys/_types.h \
+ ../../../include/sys/lock.h \
+ /opt/Espressif/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../../include/user_config.h ../../include/lwip/mem.h \
+ ../../include/mem_manager.h ../../include/lwip/raw.h \
+ ../../include/lwip/pbuf.h ../../include/lwip/err.h \
+ ../../include/lwip/def.h ../../include/lwip/ip.h \
+ ../../include/lwip/ip_addr.h ../../include/lwip/netif.h \
+ ../../include/lwip/icmp.h ../../include/lwip/sys.h \
+ ../../../include/eagle_soc.h ../../include/lwip/timers.h \
+ ../../include/lwip/inet_chksum.h ../../../include/os_type.h \
+ ../../../include/ets_sys.h ../../include/lwip/app/ping.h \
+ ../../include/espmissingincludes.h ../../include/lwip/ip_addr.h \
+ ../../../include/user_interface.h ../../../include/os_type.h \
+ ../../../include/queue.h ../../../include/spi_flash.h
